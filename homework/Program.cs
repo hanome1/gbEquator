733859fac3636508d1fdc0539cdbe4ebbc6f59f5
nick <divanchmo@gmail.com>
2023-02-11 00:48:44 +0300

    template fin

--- homework/Program.cs
+++ homework/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string ReadData(string msg) //чтение данных
+{
+    Console.WriteLine(msg);
+    string item = Console.ReadLine();
+    return item;
+}
+void PrintArray(string[] arr)  //печатаем массив красиво
+{
+    Console.Write("\n[ ");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + " ]");
+}
+string[] FillArray(string[] arr)
+{
+    
+}
+string[] ShowShortItems(string[] arr) //считает положительные числа в массиве и печатает его. требует указать кол-во элементов (n)
+{
+   
+}
